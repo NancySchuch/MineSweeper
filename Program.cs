8b314eafe802bdd1029121932c8f8d76c50de363
NancySchuch <nancy.schuch@live.se>
2021-09-13 21:29:48 +0200

    Initial commit OP1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using MineSweeper;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MineSweeper.MineSweeper mineSweeper = new MineSweeper.MineSweeper(args);
+        mineSweeper.Run();
+    }
+}
+
